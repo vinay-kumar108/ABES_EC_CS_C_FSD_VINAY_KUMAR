14a5c45a55579dbea517493df2decb42ce66a2de
vinay-kumar-108 <vinay20010920@gmail.com>
2025-11-28 13:12:40 +0530

    backened

--- day_16/backend/index.js
+++ day_16/backend/index.js
@@ -6,15 +6,30 @@ const PORT = 8800;
 
 app.use(express.json());
 
-const readData = ()=> {
-   const data=  fs.readFileSync("./data.json","utf-8");
+const readData = () => {
+    const data = fs.readFileSync("./data.json", "utf-8");
     return JSON.parse(data);
-}
+};
 
-app.get("/users", (req,res)=> {
+const writeData = (data) => {
+    fs.writeFileSync('./data.json', JSON.stringify(data, null, 2));
+};
+
+app.get("/users", (req, res) => {
     res.json(readData());
-})
+});
+
+app.post("/users", (req, res) => {
+    const fileData = readData();
+    const data = req.body;
+
+    const newData = { id: fileData.length + 1, ...data };
+    fileData.push(newData);
+
+    writeData(fileData);
+    res.status(201).json({ message: "successfully created!" });
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running http://localhost:${PORT}`);
+    console.log(`server is running at http://localhost:${PORT}`);
 });
